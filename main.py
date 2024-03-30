830677172a9a91ee71bcf58d1b4e3641cc1eaccf
Curtis Bunch <me@cudabu.io>
2024-03-29 21:58:07 -0500

    add report

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ def main():
     num_words = get_num_words(text)
     unique_characters = count_num_letters(text)
     character_count = count_characters(text)
-    print(f"{num_words} words found in the document")
-    print(f"{unique_characters} unique characters found in the document")
-    print(f"Character count: {character_count}")
+    report = generate_report(num_words, character_count)
+    print(report)
 
 def get_num_words(text):
     words = text.split()
@@ -26,18 +25,27 @@ def get_book_text(path):
 
 # count the number of unique characters in the text
 def count_num_letters(text):
-    unique_characters = set(text)
+    unique_characters = set(text.lower())
     return len(unique_characters)
 
-# count the number of times each character appears in the text
+# count the number of each character in the text ignoring case and special characters and spaces
 def count_characters(text):
     character_count = {}
     for character in text.lower():
-        if character in character_count:
-            character_count[character] += 1
-        else:
-            character_count[character] = 1
+        if character.isalpha():
+            if character in character_count:
+                character_count[character] += 1
+            else:
+                character_count[character] = 1
     return character_count
+
+# create a report that aggrfegate all the word and character data into a nice report and print it to the console
+def generate_report(num_words, character_count):
+    report = f"{num_words} words found in the document\n"
+    report += "Character count: \n"
+    for character, count in character_count.items():
+        report += f"The '{character}' character appears {count} times\n"
+    return report
 
 if __name__ == '__main__':
     main()
